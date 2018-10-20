6c7aae9ea9fa10e2104104c6ad7b1cacc47fcf40
Tina Wu <tinawu98@gmail.com>
2018-10-20 05:27:58 -0400

    Sliders of emotions storing values
    Values array contains the values of each of the slider bars

--- app/src/main/java/com/ohioguidestone/team2/profilePage2.java
+++ app/src/main/java/com/ohioguidestone/team2/profilePage2.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
 import android.widget.SeekBar;
+import android.util.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,6 +16,7 @@ public class profilePage2 extends AppCompatActivity {
     ExpandableListView expListView;
     List<String> listDataHeader;
     HashMap<String, SeekBar> listDataChild;
+    int[] values;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +35,125 @@ public class profilePage2 extends AppCompatActivity {
     private void createListData(){
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, SeekBar>();
+        values = new int[5];
 
         // Adding child data
         listDataHeader.add("Happy");
         listDataHeader.add("Sad");
         listDataHeader.add("Impulsive");
         listDataHeader.add("Anxious");
-        listDataHeader.add("Out of Control");s
+        listDataHeader.add("Out of Control");
 
         // Adding child data
         SeekBar happy = new SeekBar(this);
+        happy.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                values[0] = (progress/10);
+            }
+        });
+
 
         SeekBar sad = new SeekBar(this);
         //List<SeekBar> sad = new ArrayList<SeekBar>();
         //sad.add((SeekBar)findViewById(R.id.seekBar));
+        sad.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                values[1] = (progress/10);
+            }
+        });
 
         SeekBar impulsive = new SeekBar(this);
         //List<SeekBar> impulsive = new ArrayList<SeekBar>();
         //impulsive.add((SeekBar)findViewById(R.id.seekBar));
+        impulsive.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                values[2] = (progress/10);
+            }
+        });
 
         SeekBar anxious = new SeekBar(this);
         //List<SeekBar> anxious = new ArrayList<SeekBar>();
         //anxious.add((SeekBar)findViewById(R.id.seekBar));
+        happy.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                values[3] = (progress/10);
+            }
+        });
 
         SeekBar control = new SeekBar(this);
         //List<SeekBar> control = new ArrayList<SeekBar>();
         //control.add((SeekBar)findViewById(R.id.seekBar));
+        happy.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                values[4] = (progress/10);
+            }
+        });
+
 
         listDataChild.put(listDataHeader.get(0), happy); // Header, Child data
         listDataChild.put(listDataHeader.get(1), sad);
